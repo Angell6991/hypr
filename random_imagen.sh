6c54eb277966aedbcdd0f1dda5c1348f8a23a26c
Angell6991 <miguel9196.guzman@gmail.com>
2026-04-06 17:07:31 -0500

    awww random

--- random_imagen.sh
+++ random_imagen.sh
@@ -8,6 +8,25 @@ WALLPAPER_DIR="$HOME/Images/.wallpaper/hypr/"
 # Selecciona una imagen aleatoria (ajusta extensión si tienes otros formatos)
 WALLPAPER=$(find "$WALLPAPER_DIR" -type f \( -iname '*.jpg' -o -iname '*.png' \) | shuf -n 1)
 
+
+# tipos de transitions 
+TYPE_LIST=("simple" "outer" "center" "wipe" "top" "bottom" "left" "right")
+TYPE=${TYPE_LIST[$(( RANDOM % ${#TYPE_LIST[@]} ))]}
+
+
+# positions transitions
+POS_LIST=("0,0" "0.5,0.5" "0.999,0" "0.999,0.999" "0,0.999" "0.2,0.4" "0.8,0.8" "0.3,0.3")
+POS=${POS_LIST[$(( RANDOM % ${#TYPE_LIST[@]} ))]}
+
+
+# angle transitions
+ANGLE_LIST=("30" "120" "210" "300" "15" "135" "225" "315")
+ANGLE=${ANGLE_LIST[$(( RANDOM % ${#TYPE_LIST[@]} ))]}
+
+
 # Poner el wallpaper con swww
 awww-daemon 
-awww img "$WALLPAPER" --transition-type=any --transition-duration=2 
+awww img "$WALLPAPER" --transition-type="$TYPE" --transition-pos="$POS" --transition-angle="$ANGLE" --transition-duration=2 &
+
+
+
